56a96cb5fbf791201b2fbe006e80fee40dd2661b
IvanL <antila3567@gmail.com>
2023-12-26 14:58:13 +0200

    Initial Commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Иван Легенький on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
